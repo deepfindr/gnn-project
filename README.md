947e511bb58097a3f76c78b76859a19f57dac893
deepfindr <deepfindr@gmail.com>
2021-05-07 13:55:57 +0200

    smaller refinements

--- README.md
+++ README.md
@@ -11,14 +11,17 @@ If you run on Ubuntu / WSL you can simply run:
 ```
 sudo apt-get install python-rdkit
 ```
+Ideally execute the code in an anaconda environment, that's the easiest solution with rdkit.
 
 ## Installing the other packages
 For pytorch geometric follow this tutorial:
 https://pytorch-geometric.readthedocs.io/en/latest/notes/installation.html
 
-
+Make sure your CUDA version as well as torch version match the PyG version you install.
+I've used torch 1.6.0 as it seemed to be most stable with the other libraries.
 
 ## Further things
-Its highly recommended to setup a GPU (including CUDA) for this code. 
-
-Here is where I found ideas for node / edge features: https://www.researchgate.net/figure/Descriptions-of-node-and-edge-features_tbl1_339424976
+- Its highly recommended to setup a GPU (including CUDA) for this code. 
+- Here is where I found ideas for node / edge features: https://www.researchgate.net/figure/Descriptions-of-node-and-edge-features_tbl1_339424976
+- There is also a Kaggle competition that used this dataset (from a University):
+https://www.kaggle.com/c/iml2019/overview
--- dataset.py
+++ dataset.py
@@ -1,8 +1,12 @@
 #%% Load Dataframe
 import pandas as pd
-DATA_PATH = "data/HIV.csv"
+DATA_PATH = "data/raw/HIV.csv"
 data = pd.read_csv(DATA_PATH)
 data.head()
+
+#%% General information about the dataset
+print(data.shape)
+print(data["HIV_active"].value_counts())
 
 #%% Show sample molecules
 import rdkit
@@ -19,43 +23,64 @@ grid
 
 #%% Quick check with versions
 import torch
+import torch_geometric
 print(f"Torch version: {torch.__version__}")
 print(f"Cuda available: {torch.cuda.is_available()}")
+print(f"Torch geometric version: {torch_geometric.__version__}")
 
 #%% Generate a dataset
 from torch_geometric.data import Dataset, Data
 import numpy as np 
 import os
 from rdkit.Chem import rdmolops
-from scipy import sparse
+from tqdm import tqdm
 
 class MoleculeDataset(Dataset):
-    def __init__(self, data_path):
-        self.processed_dir = "data/processed/"
-        self.data_path = data_path
-        self.data = pd.read_csv(self.data_path)
+    def __init__(self, root, transform=None, pre_transform=None):
+        """
+        root = Where the dataset should be stored. This folder is split
+        into raw_dir (downloaded dataset) and processed_dir (processed data). 
+        """
+        super(MoleculeDataset, self).__init__(root, transform, pre_transform)
+        
+    @property
+    def raw_file_names(self):
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)  
+        """
+        return 'HIV.csv'
+
+    @property
+    def processed_file_names(self):
+        """ If these files are found in raw_dir, processing is skipped"""
+        return 'not_implemented.pt'
+
+    def download(self):
+        pass
 
     def process(self):
-        for index, mol in self.data.iterrows():
-            mol = Chem.MolFromSmiles(mol["smiles"])
+        self.data = pd.read_csv(self.raw_paths[0])
+        for index, mol in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+            mol_obj = Chem.MolFromSmiles(mol["smiles"])
             # Get node features
-            node_feats = self._get_node_features(mol)
+            node_feats = self._get_node_features(mol_obj)
             # Get edge features
-            edge_feats = self._get_edge_features(mol)
+            edge_feats = self._get_edge_features(mol_obj)
             # Get adjacency info
-            edge_index = self._get_adjacency_info(mol)
+            edge_index = self._get_adjacency_info(mol_obj)
+            # Get labels info
+            label = self._get_labels(mol["HIV_active"])
 
             # Create data object
             data = Data(x=node_feats, 
                         edge_index=edge_index,
                         edge_attr=edge_feats,
-                        y=[mol["HIV_active"]], # Graph-level labels
+                        y=label,
                         smiles=mol["smiles"]
                         ) 
             torch.save(data, 
                     os.path.join(self.processed_dir, 
                                  f'data_{index}.pt'))
-
 
     def _get_node_features(self, mol):
         """ 
@@ -78,9 +103,9 @@ class MoleculeDataset(Dataset):
             node_feats.append(atom.GetIsAromatic())
             # Append node features to matrix
             all_node_feats.append(node_feats)
-        
-        return np.asarray(all_node_feats)
 
+        all_node_feats = np.asarray(all_node_feats)
+        return torch.tensor(all_node_feats, dtype=torch.float)
 
     def _get_edge_features(self, mol):
         """ 
@@ -98,29 +123,37 @@ class MoleculeDataset(Dataset):
             # Append node features to matrix
             all_edge_feats.append(edge_feats)
 
-        return np.asarray(all_edge_feats)
-
+        all_edge_feats = np.asarray(all_edge_feats)
+        return torch.tensor(all_edge_feats, dtype=torch.float)
 
     def _get_adjacency_info(self, mol):
         adj_matrix = rdmolops.GetAdjacencyMatrix(mol)
         row, col = np.where(adj_matrix)
         coo = np.array(list(zip(row, col)))
         coo = np.reshape(coo, (2, -1))
-        return coo
+        return torch.tensor(coo, dtype=torch.long)
 
+    def _get_labels(self, label):
+        label = np.asarray([label])
+        return torch.tensor(label, dtype=torch.int64)
 
     def len(self):
-        return len(self.data.shape[0])
+        return self.data.shape[0]
 
     def get(self, idx):
-        """ Equivalent to __getitem__ in pytorch """
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
+        """
         data = torch.load(os.path.join(self.processed_dir, 
                                  f'data_{idx}.pt'))
         return data
 
-
-
-
 #%% Test the dataset
-dataset = MoleculeDataset(data_path="data/HIV.csv")
-dataset[0]
+dataset = MoleculeDataset(root="data/")
+
+# %%
+print(dataset[0].edge_index.t())
+print(dataset[0].x)
+print(dataset[0].edge_attr)
+print(dataset[0].y)
+# %%
